4fe492abec5df47146a699609e582cdab4ad683c
juanclucellas <clucellas88@gmail.com>
2024-05-09 23:01:37 -0300

    Primer trabajo

--- js/main.js
+++ js/main.js
@@ -1,19 +1,38 @@
 console.log("hola, mundi"); 
 
+//let peso = Number(prompt("Escriba su peso en Kg"));
+//let altura = Number(prompt("Escriba su altura en METROS"));
+
+//function mostrarIMC(resultado){
+    //alert("Tu indice de masa corporal es; " + resultado)
+      //  if (resultado <18.)5{
+        //alert("Estas delgado")
+    //}else if(resultado <=25){
+      //  alert("Tu peso es normal")   
+   // }else if(resultado <= 30){
+  //      alert("Tienes sobrepeso")
+  //  }else{ (resultado >=31)
+  //      alert("Tienes obesidad")
+   // };
+   // return resultado
+//}
 let peso = Number(prompt("Escriba su peso en Kg"));
 let altura = Number(prompt("Escriba su altura en METROS"));
-let resultado = peso/altura 
-let resultado1 = resultado/altura
 
-if (resultado1 <18.5){
-    alert("Estas bajo de peso")   
-}else if(resultado1 <=25){
-    alert("Peso normal")   
-}else if(resultado1 <= 30){
-    alert("Tienes sobrepeso")
-}else{ (resultado1 >=31)
-    alert("Obeso")
-};
+function IndicadorIMC (peso, altura){
+    return peso/(altura*altura);
+}
 
-console.log(resultado1)
+const Resultado = IndicadorIMC(peso,altura);
 
+if (Resultado <18.5){
+    alert ("Tu IMC es: " + Resultado.toFixed(2) + ", Estas delgado")
+}else if (Resultado <=25){
+    alert ("Tu IMC es: " + Resultado.toFixed(2) + ", Tu peso es normal")
+}else if (Resultado <=30){
+    alert("Tu IMC es: " + Resultado.toFixed(2) + ", Tienes sobre peso")
+}else if (Resultado >=31)
+    alert ("Tu ICM es: " + Resultado.toFixed(2) + ", Tienes obesidad")
+;
+
+console.log(Resultado)
